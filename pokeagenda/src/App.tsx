7b92fd31e568fb8d779298ccb69ec5d009ae2811
Carlos Rodrigues <crodrigsantos@gmail.com>
2021-08-08 21:03:44 -0300

    criação do projeto

---
+++ pokeagenda/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './global.css';
+
+
+
+ function App() {
+  return (
+   <>
+   <h1>Eloiza Santos e Carlinhos 👏👏👏 </h1>
+   <img src="../images/Elo e Carlinhos.jpg" alt="Elo e Carlinhos" />
+   </>
+  );
+}
+
+export default App;
