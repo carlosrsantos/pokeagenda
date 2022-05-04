4055fb14c64593fd9866e3443ea566798ea5e4b1
Carlos Rodrigues <crodrigsantos@gmail.com>
2022-05-03 23:26:30 -0300

    exibição de tipos de pokémon

--- pokeagenda/src/components/CardPokemon/index.tsx
+++ pokeagenda/src/components/CardPokemon/index.tsx
@@ -1,103 +1,96 @@
 import { useEffect, useState } from "react";
 import { pokeApi } from "../../services/api";
-import './styles.css';
+import "./styles.css";
 
-interface Pokemon{
-  id: number; 
+interface TypePokemon {
+  slot: any;
+  type: {
+    name: string;
+  };
+}
+interface Pokemon {
+  id: number;
   name: string;
   picture: string;
-  types: { type: { name: string }}[];
-  
+  types: TypePokemon[];
 }
 
-export default function CardPokemon(){
+export default function CardPokemon() {
   const [pokemon, setPokemon] = useState<Pokemon>({
-    id:0,
-    name: '',
-    picture: '',
+    id: 0,
+    name: "",
+    picture: "",
     types: [],
-  
   });
 
-  useEffect(()=>{
-    pokeApi.get(`/1`).then((p => {
+  useEffect(() => {
+    pokeApi.get(`/1`).then((p) => {
       setPokemon({
-        id:p.data.id,
-        name: p.data.name,       
+        id: p.data.id,
+        name: p.data.name,
         picture: p.data.sprites.other.dream_world.front_default,
         types: p.data.types,
-      
-    })
-  }
-  ));      
-  },[]);
+      });
+    });
+  }, []);
 
-  async function anteriorPokemon () {
+  async function anteriorPokemon() {
     let pokeId = pokemon.id;
 
-    if(pokemon.id >= 1) {
-      pokeId-=1;
-      pokeApi.get(`/${pokeId}`).then((p => {
+    if (pokemon.id >= 1) {
+      pokeId -= 1;
+      pokeApi.get(`/${pokeId}`).then((p) => {
         setPokemon({
-          id:p.data.id,
-          name: p.data.name,       
+          id: p.data.id,
+          name: p.data.name,
           picture: p.data.sprites.other.dream_world.front_default,
           types: p.data.types,
-          
-      })
+        });
+      });
+    } else {
+      document.getElementById("anterior")?.setAttribute("disabled", "true");
     }
-    ));      
-  }
-  else {
-    document.getElementById('anterior')?.setAttribute("disabled","true")
-
-    }
-
   }
 
-  async function proximoPokemon () {
+  async function proximoPokemon() {
     let pokeId = pokemon.id;
 
-   if(pokemon.id < 151) {
-      pokeId+=1;
+    if (pokemon.id < 151) {
+      pokeId += 1;
 
-      pokeApi.get(`/${pokeId}`).then((p => {
+      pokeApi.get(`/${pokeId}`).then((p) => {
         setPokemon({
-          id:p.data.id,
-          name: p.data.name,       
+          id: p.data.id,
+          name: p.data.name,
           picture: p.data.sprites.other.dream_world.front_default,
           types: p.data.types,
-          // type_secondary: p.data.types[1].type.name,
-      })
+        });
+      });
     }
-    )); 
- }
-}
-  
+  }
 
   return (
     <main>
       <div className="card">
-        <h3>{`${pokemon.name} #${pokemon.id}` }</h3>
+        <h3>{`${pokemon.name} #${pokemon.id}`}</h3>
 
-        <img className="sprite" src={pokemon.picture} alt={pokemon.name} /> 
-         {/* Types:
-         {pokemon.types.length > 1 ? <p>{pokemon.types[0].type.name} & {pokemon.types[1].type.name}</p>
-           : <p>{pokemon.types[0].type.name}</p>
-         } */}
+        <img className="sprite" src={pokemon.picture} alt={pokemon.name} />
+         <h4>Types:</h4> 
+          {pokemon.types.length > 0 && (
+            <ul>
+              {pokemon.types.map((type) => (
+              <li key={type.slot}>{type.type.name}</li>
+              ))}
+            </ul>
+          )}
         
-        
+
         <div className="buttons">
-          
           <button id="anterior" onClick={anteriorPokemon}>
-            <span className="material-icons">
-            arrow_back
-            </span>
+            <span className="material-icons">arrow_back</span>
           </button>
           <button onClick={proximoPokemon}>
-            <span className="material-icons">
-            arrow_forward
-            </span>
+            <span className="material-icons">arrow_forward</span>
           </button>
         </div>
       </div>
